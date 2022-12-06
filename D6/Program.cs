f3ab9ab75d94a4018c61e0dd1f392c6346245824
Je-RICO-h <98887980+Je-RICO-h@users.noreply.github.com>
2022-12-06 15:15:50 +0100

    Added Day 6
    Today's Language choice was C#, I think it was an easy task without effort.

---
+++ D6/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using static System.Console;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SampleApp
+{
+    class Program
+    {
+        static int counter(String data, int tracer_end)
+        {
+            int tracer_begin = 0;
+
+            while (tracer_end != data.Count())
+            {
+                bool is_unique = true;
+
+                foreach (char c in data[tracer_begin..tracer_end])
+                {
+                    int char_count = data[tracer_begin..tracer_end].ToCharArray().Count(c2 => c2 == c);
+
+                    if (char_count != 1)
+                    {
+                        is_unique = false;
+                        break;
+                    }
+                }
+
+                if (is_unique)
+                    return tracer_end;
+                else
+                {
+                    tracer_begin++;
+                    tracer_end++;
+                }
+            }
+
+            return -1;
+        }
+
+        public static void Main(string[] args)
+        {
+            StreamReader fr = new StreamReader("input.txt");
+
+            String data;
+
+            data = fr.ReadToEnd();
+
+            fr.Close();
+
+            WriteLine("Part1: " + counter(data, 4));
+            WriteLine("Part2: " + counter(data, 14));
+        }
+    }
+}
